40aba9c46b1baf756b2441803fe386be0cdf94f8
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2025-03-22 22:01:01 +0800

    fix: add missing newline

--- utils/error.c
+++ utils/error.c
@@ -4,7 +4,7 @@ int handle_error(int return_val, char *error_message, char *file, int line_no) {
   if (return_val < 0) {
     fprintf(
       stderr,
-      "%s\nfile: %s\nline number: %d\nerror: %s (%d)",
+      "%s\nfile: %s\nline number: %d\nerror: %s (%d)\n",
       error_message,
       file,
       line_no,
